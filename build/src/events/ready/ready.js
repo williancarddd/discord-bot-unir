cae0dba742399f8d4c4b1e59f7de6e5c5abe6bad
William Cardoso <williancard123@gmail.com>
2022-09-03 17:25:55 -0400

    test4

--- build/src/events/ready/ready.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Event_1 = require("../../structures/Event");
-exports.default = new Event_1.Event('ready', () => {
-    console.log('Bot is ready.');
-});
